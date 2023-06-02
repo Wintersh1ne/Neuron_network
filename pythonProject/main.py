b840d03d89e8ffaa939304936c00feb871eae81f
Wintersh1ne <130225456+Wintersh1ne@users.noreply.github.com>
2023-06-02 10:20:02 +0300

    update

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def deriv_sigmoid(x):
     # Производная от sigmoid: f'(x) = f(x) * (1 - f(x))
@@ -15,105 +17,108 @@ def mse_loss(y_true, y_pred):
 
 
 class OurNeuralNetwork:
-    """
-    Нейронная сеть, у которой:
-        - 2 входа                                   #w_count
-        - скрытый слой с двумя нейронами (h1, h2)   #b_count - 1
-        - слой вывода с одним нейроном (o1)         #1
+    def __init__(self, w_count_in, b_count):
+        self.w_count_in = w_count_in
+        self.w_count = w_count_in * (b_count - 1) + b_count - 1
+        self.w = []
+        self.b = []
+        self.h = [0] * (b_count - 1)
+        self.d_ypred_d_h = [0] * len(self.h)
+        self.d_ypred_d_w = [0] * len(self.h)
+        self.d_ypred_d_b = 0
+        self.d_h_d_w = [0] * len(self.h) * w_count_in
+        self.d_h_d_b = [0] * len(self.h)
 
-    *** ВАЖНО ***:
-    Код ниже написан как простой, образовательный. НЕ оптимальный.
-    Настоящий код нейронной сети выглядит не так. НЕ ИСПОЛЬЗУЙТЕ этот код.
-    Вместо этого, прочитайте/запустите его, чтобы понять, как работает эта сеть.
-    """
+        for i in range(0, self.w_count):
+            self.w.append(np.random.normal())
 
-    def __init__(self):
-        # Вес
-
-        self.w1 = np.random.normal()
-        self.w2 = np.random.normal()
-        self.w3 = np.random.normal()
-        self.w4 = np.random.normal()
-        self.w5 = np.random.normal()
-        self.w6 = np.random.normal()
-
-        # Смещения
-        self.b1 = np.random.normal()
-        self.b2 = np.random.normal()
-        self.b3 = np.random.normal()
+        for i in range(0, b_count):
+            self.b.append(np.random.normal())
 
     def feedforward(self, x):
-        # x является массивом numpy с двумя элементами
-        h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-        h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-        o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
-        return o1
+        k = 0
+        for i in range(0, len(self.b) - 1):
+            summ = 0
+            for j in range(0, self.w_count_in):
+                summ += self.w[j + k] * x[j]
+            k += self.w_count_in
+            self.h[i] = (sigmoid(summ + self.b[i]))
+
+        summ = 0
+        for i in range(0, len(self.b) - 1):
+            summ += self.w[len(self.w) - len(self.b) + i] * self.h[i]
+        o = sigmoid(summ + self.b[-1])
+
+        return o
 
     def train(self, data, all_y_trues):
-        """
-        - data is a (n x 2) numpy array, n = # of samples in the dataset.
-        - all_y_trues is a numpy array with n elements.
-            Elements in all_y_trues correspond to those in data.
-        """
-        learn_rate = 0.1
-        epochs = 1000  # количество циклов во всём наборе данных
+        learn_rate = 0.5
+        epochs = 10000
 
         for epoch in range(epochs):
             for x, y_true in zip(data, all_y_trues):
-                # --- Выполняем обратную связь (нам понадобятся эти значения в дальнейшем)
-                sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
-                h1 = sigmoid(sum_h1)
 
-                sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
-                h2 = sigmoid(sum_h2)
+                sums = [0] * len(self.b)
+                for i in range(0, len(self.b) - 1):
+                    for j in range(0, len(self.w) // len(self.b)):
+                        sums[i] += self.w[j + j * (len(self.w) // len(self.b))] * x[j]
+                    self.h[i] = (sigmoid(sums[i] + self.b[i]))
 
-                sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
-                o1 = sigmoid(sum_o1)
-                y_pred = o1
+                # +++
+                for i in range(0, len(self.b) - 1):
+                    sums[-1] += self.w[len(self.w) - len(self.b) - 1 + i] * self.h[i]
+                o = sigmoid(sums[-1] + self.b[-1])
+
+                y_pred = o
 
                 # --- Подсчет частных производных
                 # --- Наименование: d_L_d_w1 представляет "частично L / частично w1"
                 d_L_d_ypred = -2 * (y_true - y_pred)
 
-                # Нейрон o1
-                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
-                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
-                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+                # h
+                j, k = 0, 0
+                for i in range(0, len(self.d_h_d_w)):
+                    self.d_h_d_w[i] = x[j] * deriv_sigmoid(sums[k])
+                    j += 1
+                    if j == len(x):
+                        j = 0
+                        k += 1
 
-                d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
-                d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+                for i in range(0, len(self.d_h_d_b)):
+                    self.d_h_d_b[i] = deriv_sigmoid(sums[i])
 
-                # Нейрон h1
-                d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
-                d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
-                d_h1_d_b1 = deriv_sigmoid(sum_h1)
+                # o
+                for i in range(0, len(self.h)):
+                    self.d_ypred_d_w[i] = (self.h[i] * deriv_sigmoid(sums[-1]))
 
-                # Нейрон h2
-                d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
-                d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
-                d_h2_d_b2 = deriv_sigmoid(sum_h2)
+                j = 0
+                for i in range(self.w_count - self.w_count_in, self.w_count):
+                    self.d_ypred_d_h[j] = (self.w[i] * deriv_sigmoid(sums[-1]))
+                    j += 1
 
-                # --- Обновляем вес и смещения
-                # Нейрон h1
-                self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
-                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
-                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+                d_ypred_d_b = deriv_sigmoid(sums[-1])
 
-                # Нейрон h2
-                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
-                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
-                self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
+                j, c = 0, 0
+                for i in range(0, self.w_count - self.w_count_in):
+                    if c > self.w_count_in:
+                        self.b[j] -= learn_rate * d_L_d_ypred * self.d_ypred_d_h[j] * self.d_h_d_b[j]
+                        j += 1
+                        c = 0
+                    self.w[i] -= learn_rate * d_L_d_ypred * self.d_ypred_d_h[j] * self.d_h_d_w[i]
 
-                # Нейрон o1
-                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+                    c += 1
+
+                j = 0
+                for i in range(self.w_count - self.w_count_in, self.w_count):
+                    self.w[i] -= learn_rate * d_L_d_ypred * self.d_ypred_d_w[j]
+                    j += 1
+                self.b[-1] -= learn_rate * d_L_d_ypred * d_ypred_d_b
 
             # --- Подсчитываем общую потерю в конце каждой фазы
             if epoch % 10 == 0:
                 y_preds = np.apply_along_axis(self.feedforward, 1, data)
                 loss = mse_loss(all_y_trues, y_preds)
-                print("Epoch %d loss: %.3f" % (epoch, loss))
+                print(f"Epoch {epoch} loss: {loss}")
 
 
 # Определение набора данных
@@ -131,12 +136,11 @@ all_y_trues = np.array([
     1,  # Diana
 ])
 
-# Тренируем нашу нейронную сеть!
-network = OurNeuralNetwork()
+# Тренируем нейронную сеть
+network = OurNeuralNetwork(2, 3)
 network.train(data, all_y_trues)
 
-# Делаем предсказания
-emily = np.array([-7, -3])  # 128 фунтов, 63 дюйма
-frank = np.array([20, 2])  # 155 фунтов, 68 дюймов
-print("Emily: ",  network.feedforward(emily))  # 0.951 - F
-print("Frank: ",  network.feedforward(frank))  # 0.039 - M
+emily = np.array([-7, -3])
+frank = np.array([20, 2])
+print("Emily: %.9f" % network.feedforward(emily))  # 0.951 - F
+print("Frank: %.9f" % network.feedforward(frank))  # 0.039 - M
